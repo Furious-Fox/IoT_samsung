923cd01372780641e3c9afd48c2511aa18f2d8e1
VM <Tosvering@gmail.com>
2020-08-15 17:42:46 +0700

    v0.3 Add write&read in files

--- main.py
+++ main.py
@@ -1,17 +1,66 @@
 import hashlib
+import sys
+import os.path
+
+basePath = "/home/vm/IoT_samsung/Cells/"
+numCells = 3
 
 # Part w/ big block of code to open the cell
 def OpenCell():
     print("Open your cell!")
 
+def WriteHashInFile(fileName ,hashStr):
+    try:
+        fileObj = open(basePath + fileName, "w")
+        fileObj.write(hashStr)
+    except Exception as ex:
+        print("Cant open and write to file :" + fileName, file = sys.stderr)
+        print(ex, file = sys.stderr)
+    finally:
+        fileObj.close()
+ 
+def CheckCellFile(fileName):
+    filePath = basePath + fileName
+    if os.path.exists(filePath):
+        return True
+    else:
+        return False
+
+def GetHashFromFile(fileName):
+    try:
+        fileObj = open(basePath + fileName, "r")
+        lineFile = fileObj.readline()
+    except Exception as ex:
+        print("Cant open and read file: " + fileName, file = sys.stderr)
+        print(ex, file = sys.stderr)
+    finally:
+        fileObj.close()
+    
+    return lineFile
+
+
 def main():
+    # Debug init
+    WriteHashInFile("01", "781e5e245d69b566979b86e28d23f2c7")
+    WriteHashInFile("02", "781e5e245d69b566979b86e28d23f2c7")
+    WriteHashInFile("03", "781e5e245d69b566979b86e28d23f2c7")
+    #
 
     userStr = input("Enter your code:")
+    cellNumStr = userStr[:2]
 
-    checkStr = "827ccb0eea8a706c4c34a16891f84e7b"   # TODO: Move it in the file
-    hashStrObj = hashlib.md5(userStr.encode())
+    # Maybe we need it
+    #if cellNumStr[0] == '0':
+    #    cellNumInt = int(cellNumStr[1])
+    
+    userStr = userStr[2:]
 
-    if hashStrObj.hexdigest() == checkStr:
+    if(CheckCellFile(cellNumStr)):
+        hashFileStr = GetHashFromFile(cellNumStr)
+
+    userStr = hashlib.md5(userStr.encode())
+
+    if userStr.hexdigest() == hashFileStr:
         OpenCell()
     else:
         print("Code check error!")
