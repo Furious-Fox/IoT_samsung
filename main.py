019fb220ccad11880391fa3a88a55a81222b5087
VM <Tosvering@gmail.com>
2020-08-15 15:46:26 +0700

    v0.2 Add md5 hash

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import hashlib
+
+# Part w/ big block of code to open the cell
 def OpenCell():
     print("Open your cell!")
 
+checkStr = "827ccb0eea8a706c4c34a16891f84e7b"   # TODO: Move it in the file
 userStr = input("Enter your code:")
 
-checkStr = "12345"
+hashStrObj = hashlib.md5(userStr.encode())
 
-if userStr == checkStr:
+if hashStrObj.hexdigest() == checkStr:
     OpenCell()
 else:
     print("Code check error!")
